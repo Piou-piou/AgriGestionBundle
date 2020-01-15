b57a7c88feec0b87a98e83241c0c4a776a3adbe6
Piou-piou <pilloud.anthony@gmail.com>
2020-01-15 17:15:29 +0100

    [func] add services yml config for services

--- AgriGestionBundle.php
+++ AgriGestionBundle.php
@@ -2,8 +2,13 @@
 
 namespace PiouPiou\AgriGestionBundle;
 
+use PiouPiou\AgriGestionBundle\DependencyInjection\AgriGestionExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AgriGestionBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new AgriGestionExtension();
+    }
 }
