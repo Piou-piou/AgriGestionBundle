e0f3d776a4a3919086eb670d3d99ece441a86865
Piou-piou <pilloud.anthony@gmail.com>
2020-01-16 09:03:01 +0100

    [func] add list of all providers

--- Controller/ProviderController.php
+++ Controller/ProviderController.php
@@ -2,6 +2,7 @@
 
 namespace PiouPiou\AgriGestionBundle\Controller;
 
+use PiouPiou\AgriGestionBundle\Entity\Provider;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,16 +10,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProviderController extends AbstractController
 {
     /**
-     * @Route("/providers/list", name="agrigestion_admin_provider_index")
+     * @Route("/providers/list/", name="agrigestion_admin_provider_index")
      * @return Response
      */
     public function index():Response
     {
-        return $this->render("@AgriGestion/admin/providers/index.html.twig");
+        $providers = $this->getDoctrine()->getRepository(Provider::class)->findAll();
+
+        return $this->render("@AgriGestion/admin/providers/index.html.twig", ["providers" => $providers]);
     }
 
     /**
-     * @Route("/providers/create", name="agrigestion_admin_provider_create")
+     * @Route("/providers/create/", name="agrigestion_admin_provider_create")
+     * @Route("/providers/edit/", name="agrigestion_admin_provider_edit")
      * @return Response
      */
     public function edit():Response
