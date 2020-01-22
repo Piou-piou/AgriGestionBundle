80d75e97aa3ff1937208b1d3a1d4fb75b8910deb
Piou-piou <pilloud.anthony@gmail.com>
2020-01-22 13:42:52 +0100

    [func] add test on type of field in research

--- Controller/SearchEngineTrait.php
+++ Controller/SearchEngineTrait.php
@@ -2,14 +2,14 @@
 
 namespace PiouPiou\AgriGestionBundle\Controller;
 
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\QueryBuilder;
-use Doctrine\Persistence\ManagerRegistry;
-use Doctrine\Persistence\ObjectManager;
 
 trait SearchEngineTrait
 {
+    /** @var EntityManagerInterface */
+    private $em;
+
     /**
      * @param EntityManagerInterface $em
      * @param array $searches
@@ -18,15 +18,38 @@ trait SearchEngineTrait
      */
     public function doSearch(EntityManagerInterface $em, array $searches, string $class)
     {
+        $this->em = $em;
+        $entity_fields = $this->getEntityFields($class);
+
         /** @var QueryBuilder $query */
-        $query = $em->getRepository($class)->createQueryBuilder("query");
+        $query =  $this->em->getRepository($class)->createQueryBuilder("query");
 
         foreach ($searches as $key => $search) {
             if ($search !== "") {
-                $query->andWhere("query.".$key . " LIKE :" . $key)->setParameter(":".$key, "%".$search."%");
+                if ($entity_fields[$key] === "string") {
+                    $query->andWhere("query.".$key . " LIKE :" . $key)->setParameter(":".$key, "%".$search."%");
+                }
             }
         }
 
         return $query->getQuery()->getResult();
+    }
+
+    /**
+     * get entity fields type of given entity
+     * @param $class
+     * @return array
+     */
+    private function getEntityFields($class): array
+    {
+        $metadata = $this->em->getClassMetadata($class);
+        $entity_fields = $metadata->getFieldNames();
+        $fields = [];
+
+        foreach ($entity_fields as $entity_field) {
+            $fields[$entity_field] = $metadata->getTypeOfField($entity_field);
+        }
+
+        return $fields;
     }
 }
