eae94344dc2ba0c4733c7ed7ed22e0f23df365bb
Piou-piou <pilloud.anthony@gmail.com>
2020-02-25 07:32:13 +0100

    [func] prepare assets

---
+++ Resources/public/js/main.js
@@ -0,0 +1,28 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["js/main"],{
+
+/***/ "./assets/js/main.js":
+/*!***************************!*\
+  !*** ./assets/js/main.js ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/***/ }),
+
+/***/ 0:
+/*!*********************************!*\
+  !*** multi ./assets/js/main.js ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! ./assets/js/main.js */"./assets/js/main.js");
+
+
+/***/ })
+
+},[[0,"runtime"]]]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJqcy9tYWluLmpzIiwic291cmNlUm9vdCI6IiJ9
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var Encore = require('@symfony/webpack-encore');
+const path = require('path');
+var glob = require('glob');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
+Encore
+.enableSingleRuntimeChunk()
+.setOutputPath('./Resources/public/')
+.setPublicPath('/bundles/agrigestion')
+.setManifestKeyPrefix('bundles/agrigestion')
+
+
+// ----------------------  JS --------------------------------------//
+.addEntry('js/main', [
+  './assets/js/main.js',
+])
+
+
+// ----------------------  CSS --------------------------------------//
+
+
+// ----------------------  Other configs --------------------------------------//
+
+.addPlugin(new ProgressBarPlugin())
+
+.addLoader({
+  test: /\.js$/,
+  exclude: /node_modules/,
+  include: [
+    path.join(__dirname, 'node_modules/ribs-core/'),
+    path.join(__dirname, 'assets/'),
+  ],
+  loader: 'babel-loader',
+})
+
+.enableSassLoader()
+
+.enableSourceMaps(!Encore.isProduction());
+
+module.exports = Encore.getWebpackConfig();
