8c95dcf242f7ca55ee58a880afc26fce3f04ccfe
Piou-piou <pilloud.anthony@gmail.com>
2020-03-09 08:58:33 +0100

    [func] prepare autocomplete js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,17 @@ const ribsForms = document.querySelectorAll('form.ribs-form');
 ribsForms.forEach((form) => {
   const autocompleteFields = form.querySelectorAll('.input-autocomplete');
 
+  const div = document.createElement('div');
+  div.classList.add('result-autocomplete');
+  document.querySelector('.input-autocomplete').after(div);
+
   autocompleteFields.forEach((autocompleteField) => {
     autocompleteField.addEventListener('keyup', (event) => {
       const field = event.currentTarget;
       if (field.value.length > 2) {
         api.post(field.dataset.url, {autocomplete: field.value}, 'html').then((data) => {
-          console.log(data);
+          const autocompleteResult = autocompleteField.parentNode.querySelector('.result-autocomplete');
+          autocompleteResult.innerHTML = data;
         });
       }
     });
