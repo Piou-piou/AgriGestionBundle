de637db9a446b8237c4cd996632db2f48e189096
Piou-piou <pilloud.anthony@gmail.com>
2020-04-21 16:12:29 +0200

    [edit] method to get parcel list usage

--- Controller/ParcelsApiController.php
+++ Controller/ParcelsApiController.php
@@ -7,6 +7,7 @@ use PiouPiou\AgriGestionBundle\Entity\CowsInParcel;
 use PiouPiou\RibsAdminBundle\Service\Api;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ParcelsApiController extends AbstractController
@@ -14,16 +15,18 @@ class ParcelsApiController extends AbstractController
     /**
      * @Route("/api/parcels/list", name="agriparcel_api_admin_parcel_index")
      * @param EntityManagerInterface $em
+     * @param SessionInterface $session
      * @param Api $api
      * @return JsonResponse
      */
-    public function index(EntityManagerInterface $em, Api $api): JsonResponse
+    public function index(EntityManagerInterface $em, SessionInterface $session, Api $api): JsonResponse
     {
-        $cows_in_parcels = $em->getRepository(CowsInParcel::class);
+        $cows_in_parcels = $em->getRepository(CowsInParcel::class)->findAll();
 
         return new JsonResponse([
             "success" => true,
             "cows_in_parcel" => $api->serializeObject($cows_in_parcels),
+            "token" => $session->get("account_token")->getToken()
         ]);
     }
 }
