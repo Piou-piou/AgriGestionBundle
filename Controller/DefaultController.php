39b27d6cba4b9700c8d3e304060428a7fb062d4f
Piou-piou <pilloud.anthony@gmail.com>
2020-04-28 08:53:59 +0200

    [del] forgotten dump on parcel index

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,8 +2,6 @@
 
 namespace PiouPiou\AgriGestionBundle\Controller;
 
-use Doctrine\ORM\EntityManagerInterface;
-use PiouPiou\AgriGestionBundle\Entity\Parcel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,11 +21,8 @@ class DefaultController extends AbstractController
      * @Route("/index-parcel/", name="agriparcel_admin_index")
      * @return Response
      */
-    public function indexParcel(EntityManagerInterface $em):Response
+    public function indexParcel():Response
     {
-        $parcels = $em->getRepository(Parcel::class)->findByOldEndDate();
-        dump($parcels);
-
         return $this->render("@AgriGestion/admin/parcel/index.html.twig");
     }
 }
