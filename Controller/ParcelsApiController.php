a51292ae2afa35cb472744e32cc7000b804b4305
Piou-piou <pilloud.anthony@gmail.com>
2020-07-07 07:53:03 +0200

    [func] add method to list types of parcel and to add new parcel from app

--- Controller/ParcelsApiController.php
+++ Controller/ParcelsApiController.php
@@ -4,6 +4,7 @@ namespace PiouPiou\AgriGestionBundle\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
+use PiouPiou\AgriGestionBundle\Entity\HayTracking;
 use PiouPiou\AgriGestionBundle\Entity\Parcel;
 use PiouPiou\RibsAdminBundle\Service\Api;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -28,6 +29,60 @@ class ParcelsApiController extends AbstractController
         return new JsonResponse([
             "success" => true,
             "parcels" => $parcels,
+            "token" => $session->get("account_token")->getToken()
+        ]);
+    }
+
+    /**
+     * @Route("/parcels/list-types", name="agriparcel_api_admin_parcel_list_types", methods={"POST"})
+     * @param EntityManagerInterface $em
+     * @param SessionInterface $session
+     * @param Api $api
+     * @return JsonResponse
+     */
+    public function listTypes(EntityManagerInterface $em, SessionInterface $session, Api $api): JsonResponse
+    {
+        return new JsonResponse([
+            "success" => true,
+            "types" => Parcel::TYPES,
+            "token" => $session->get("account_token")->getToken()
+        ]);
+    }
+
+    /**
+     * @Route("/parcels/add", name="agriparcel_api_admin_haystack_add", methods={"POST"})
+     * @param EntityManagerInterface $em
+     * @param SessionInterface $session
+     * @param Api $api
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public function add(EntityManagerInterface $em, SessionInterface $session, Api $api): JsonResponse
+    {
+        $infos = $session->get("jwt_infos");
+
+        $parcel = $em->getRepository(Parcel::class)->findOneBy([
+            "name" => $infos->name,
+        ]);
+
+        if ($parcel) {
+            return new JsonResponse([
+                "success" => false,
+                "error_message" => "Cette parcelle existe déjà",
+                "token" => $session->get("account_token")->getToken()
+            ]);
+        }
+
+        $parcel = new Parcel();
+        $parcel->setName($infos->name);
+        $parcel->setType($infos->type);
+        $parcel->setSurface($infos->surface);
+        $em->persist($parcel);
+        $em->flush();
+
+        return new JsonResponse([
+            "success" => true,
+            "success_message" => "La parcelle : " .$parcel->getName()." a été créée",
             "token" => $session->get("account_token")->getToken()
         ]);
     }
